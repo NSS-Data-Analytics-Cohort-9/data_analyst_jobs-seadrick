1755b4a50adf19b68dd3ce5c95843c3c5e7dd392
seadrick <csissom12@gmail.com>
2023-06-06 21:54:37 -0500

    working on #7

--- README.md
+++ README.md
@@ -19,8 +19,10 @@ Before beginning to answer questions, take some time to review the data dictiona
 		
 
 5.	How many postings in the dataset have a review count between 500 and 1000?
+		*151*
 
 6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
+		*Nebraska*		
 
 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
 
--- SQL_scripts/DA Jobs Scripts.sql
+++ SQL_scripts/DA Jobs Scripts.sql
@@ -33,5 +33,28 @@ WHERE location = 'TN'
 --5.	How many postings in the dataset have a review count between 500 and 1000?
 SELECT * FROM data_analyst_jobs;
 
+SELECT COUNT(*)
+FROM data_analyst_jobs
+WHERE review_count BETWEEN 500 AND 1000;
+
+
+--6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
+
+SELECT * FROM data_analyst_jobs;
+
+SELECT location, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE location IS NOT NULL AND star_rating IS NOT NULL
+GROUP BY location
+ORDER BY avg_rating DESC;
+
+
+--7.	Select unique job titles from the data_analyst_jobs table. How many are there?
+
+
+
+ 
+
+
 
 
