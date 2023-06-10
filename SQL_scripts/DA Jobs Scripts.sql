64832f3a1240b2ba6458db3583780f784d94bace
seadrick <csissom12@gmail.com>
2023-06-10 10:00:59 -0500

    finsihed 1-12

--- SQL_scripts/DA Jobs Scripts.sql
+++ SQL_scripts/DA Jobs Scripts.sql
@@ -1,10 +1,11 @@
 --1.	How many rows are in the data_analyst_jobs table?
 select * from data_analyst_jobs;
 --2.	Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row?
-SELECT * FROM data_analyst_jobs
+SELECT * 
+FROM data_analyst_jobs
 LIMIT 10;
 --How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
-SELECT * FROM data_analyst_jobs;
+
 
 
 SELECT COUNT(*) AS tn_count
@@ -12,7 +13,7 @@ FROM data_analyst_jobs
 WHERE location = 'TN';
 --TN COUNT 21
 
-SELECT * FROM data_analyst_jobs;
+
 
 
 SELECT COUNT (*) as ky_tn_count
@@ -22,7 +23,7 @@ WHERE location IN ('TN', 'KY');
 
 
 --4.	How many postings in Tennessee have a star rating above 4?
-SELECT * FROM data_analyst_jobs;
+
 
 SELECT *
 FROM data_analyst_jobs
@@ -31,7 +32,6 @@ WHERE location = 'TN'
 	
 
 --5.	How many postings in the dataset have a review count between 500 and 1000?
-SELECT * FROM data_analyst_jobs;
 
 SELECT COUNT(*)
 FROM data_analyst_jobs
@@ -51,7 +51,6 @@ ORDER BY avg_rating DESC;
 
 --7.	Select unique job titles from the data_analyst_jobs table. How many are there?
 
-SELECT * FROM data_analyst_jobs;
 
 SELECT DISTINCT(title)
 FROM data_analyst_jobs;
@@ -76,32 +75,47 @@ HAVING  review_count > 5000;
 
 SELECT company, AVG(star_rating) AS avg_rating
 FROM data_analyst_jobs
-GROUP BY company, review_count
-HAVING review_count > 5000
+WHERE review_count > 5000
+AND company IS NOT NULL
+GROUP BY company
 ORDER BY avg_rating DESC;
 
 
 --11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
 SELECT DISTINCT title
 FROM data_analyst_jobs
-WHERE title LIKE '%Analyst%';
+WHERE title iLIKE '%Analyst%';
 
 
 --12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
-SELECT  title
+SELECT DISTINCT title 
 FROM data_analyst_jobs
-WHERE title NOT LIKE '%Analyst%' 
-	AND title NOT LIKE '%analyst%'
-	AND title NOT LIKE '%ANALYST%'
-	AND title NOT LIKE '%Analytics%'
-	AND title NOT LIKE '%analytics%'
-	AND title NOT LIKE '%ANALYTICS%';
+WHERE (title) NOT LIKE '%Analysts'
+AND (title) NOT LIKE '%Analytics%';
 
 
+
+
+SELECT title
+FROM data_analyst_jobs
+WHERE LOWER(title) LIKE '%analyst%'
+	OR LOWER(title) LIKE '%analytics%'
+	
+SELECT title
+FROM data_analyst_jobs
+WHERE title NOT LIKE '%analyst%'
+AND title NOT LIKE '%analytics%';
 --**BONUS:**
 --You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
  -- Disregard any postings where the domain is NULL. 
  -- Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
   -- Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4?
 
-
+SELECT domain, COUNT(days_since_posting) AS hard_to_fill_count
+FROM data_analyst_jobs
+WHERE skill LIKE '%SQL%'
+	AND days_since_posting > 21
+	AND domain IS NOT NULL 
+	GROUP BY domain
+	ORDER BY job_count
+	
